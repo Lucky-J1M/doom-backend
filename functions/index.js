fa7e37099a9f250cd605c2b6f41a1cf48c9360cc
Tom Burnley <tburnley@synamedia.com>
2025-05-18 09:17:26 -0700

    Firebase Functions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,19 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const express = require('express');
+const oasTools = require("@oas-tools/core");
+const config = require("./oastools.config.js");
+const bodyParser = require('body-parser');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const app = express();
+var serverPort = 8080;
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Define your routes
+app.use(express.json());
+app.disable('x-powered-by');
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+oasTools.initialize(app, config).then(() => {
+    http.createServer(app).listen(serverPort, () => console.log("Server started!"));
+})
+    
+// Export the Express app as a Firebase function
+exports.app = functions.https.onRequest(app);
